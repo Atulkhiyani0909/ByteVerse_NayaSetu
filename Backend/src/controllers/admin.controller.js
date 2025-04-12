e136061fdef2f2560a1e3fc1fbd76558d2efb474
Atul <atulkhiyani09@gmail.com>
2025-04-13 01:44:40 +0530

    admin controller changed made

--- Backend/src/controllers/admin.controller.js
+++ Backend/src/controllers/admin.controller.js
@@ -6,47 +6,27 @@ import mongoose from 'mongoose';
 const allWorker = async (req, res) => {
    const {id}=req.query;
     try {
-      const admin = await Admin.aggregate([
-        {
-          $match: {
-            _id: new mongoose.Types.ObjectId(id)
-          }
-        },
-        {
-          $lookup: {
-            from: 'workers',
-            localField: 'department',
-            foreignField: 'department',
-            as: 'workerInfo'
-          }
-        },
-        {
-          $unwind: {
-            path: '$workerInfo',
-            preserveNullAndEmptyArrays: true
-          }
-        },
-        {
-          $project: {
-            name: 1,
-            email: 1,
-            department: 1,
-            location: 1,
-            workerInfo: 1,
-            timestamps:1
-          }
-        }
-      ]);
+      const admin =await Admin.findById(id);
+
+      if(!admin) return "Admin Doesn't Exists"
+       
+      let city =admin.location.city;
+
+      const workers = await Worker.find({
+        department: admin.department,
+        'location.city': city , // to check for the city and department 
+      }).sort({ratings:-1}).select('-password -refreshToken');
+      
+      if(!workers) return "No Worker Found"
 
      res.status(200).json({
-        admin
+        workers
      });
     } catch (error) {
       console.log("Error in Data", error);
     }
 };
-  
 
 
-  
+
 export {allWorker}
