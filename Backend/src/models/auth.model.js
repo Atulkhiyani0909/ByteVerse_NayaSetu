8c77446f059cd87bfaabb6d864a84abf3e1c483d
Pawan <aiinnoeng@gmail.com>
2025-04-13 00:16:48 +0530

    removed mdb url

--- Backend/src/models/auth.model.js
+++ Backend/src/models/auth.model.js
@@ -1,12 +1,5 @@
 import mongoose, { Schema, model } from 'mongoose';
 
-mongoose.connect("mongodb+srv://aiinnoeng:zMj4WiAFKcIoSTZ4@learningmongodb.emg7z.mongodb.net/stories-app")
-    .then(() => {
-        console.log("Connected to MongoDB");
-    }) .catch((error) => {
-        console.error(`Error while connecting to database ${error}`);
-    });
-
 const UserSchema = new Schema({
     username : {type : String,
         required : true, 
